799ca8185b5d1d4c78ef5d1e6aad9f5e1d5542a6
sukhmeet singh <sukhmeets111@gmail.com>
2022-01-01 17:35:19 +0000

    adding insertion program

--- circular_queues.cpp
+++ circular_queues.cpp
@@ -1,87 +1,156 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class queues{
+class Queue
+{
 private:
-int rear,front;
-int n;
-int arr[];
-public:
-queues(){
-    rear=-1;
-    front=-1;
-    cout<<"enter the size of queue: ";cin>>n;
-    arr[n];
-    for (int i=0;i<n;i++){
-        arr[i]=0;
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-    void full(){
-        if(rear==n){
-            cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FIRST DEQUEUE AN ELEMENT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
-            rear=0;
-            return;
-        }
-    }
-    void empty(){
-        if(front==-1 && rear==-1){
-            front=0,rear=0;
-            return;
-        }
-        else if(front==n && rear==n){
-            rear=0;front=0;
-            return;
-        }
-    }
-    void enqueue(){
-        empty();
-        int x;
-        cout<<"enter an element to add in queue: "<<" ";cin>>x;
-        arr[rear]=x;
+    int front;
+    int rear;
+    int arr[5];
 
-        rear++;
-        full();
-        return;
+public:
+    Queue()
+    {
+        front = -1;
+        rear = -1;
+        for (int i = 0; i < 5; i++)
+        {
+            arr[i] = 0;
+        }
     }
-    void dequeue(){
-        empty();
-        int y;
-        y=arr[front];
-        arr[front]=0;
-        front++;
-        return;
+    bool isEmpty()
+    {
+        if (front == -1 && rear == -1)
+            return true;
+        else
+            return false;
     }
-    void display(){
-        for(int j=0;j<n;j++){
-            cout<<arr[j]<<" ";
+    bool isFull()
+    {
+        if ((rear+1)%5==front)
+            return true;
+        else
+            return false;
+    }
+    void enqueue(int val)
+    {
+        if (isFull())
+        {
+            cout << "Queue full" << endl;
+            return;
+        }
+        else if (isEmpty())
+        {
+            rear = 0;
+            front = 0;
+            arr[rear] = val;
+        }
+        else
+        {
+            rear = (rear + 1) % 5 ;
+            arr[rear] = val;
+        }
+    }
+
+    int dequeue()
+    {
+        int x = 0;
+        if (isEmpty())
+        {
+            cout << "Queue is Empty" << endl;
+            return x;
+        }
+        else if (rear == front)
+        {
+            x = arr[rear];
+            rear = -1;
+            front = -1;
+            return x;
+        }
+        else
+        {
+            cout << "front value: " << front << endl;
+            x = arr[front];
+            arr[front] = 0;
+            front=(front+1)%5;
+            return x;
+        }
+    }
+
+    int count()
+    {
+        return (rear - front + 1);
+    }
+
+    void display()
+    {
+        cout << "All values in the Queue are - " << endl;
+        for (int i = 0; i < 5; i++)
+        {
+            cout << arr[i] << "  ";
         }
     }
 };
-int main(){
-    queues q;
-    while(true){
-        int u;
-        cout<<"enter your choice?\tPress Ctrl+C to exit the program\n1.Enqueue\n2.Dequeue\n3.Display\n4.Clear Screen."<<endl;cin>>u;
-        switch(u){
-        case 1:{
-            q.enqueue();
+
+int main()
+{
+    Queue q1;
+    int value, option;
+
+    do
+    {
+        cout << "\n\nWhat operation do you want to perform? Select Option number. Enter 0 to exit." << endl;
+        cout << "1. Enqueue()" << endl;
+        cout << "2. Dequeue()" << endl;
+        cout << "3. isEmpty()" << endl;
+        cout << "4. isFull()" << endl;
+        cout << "5. count()" << endl;
+        cout << "6. display()" << endl;
+        cout << "7. Clear Screen" << endl
+             << endl;
+
+        cin >> option;
+
+        switch (option)
+        {
+        case 0:
             break;
-        }
-        case 2:{
-            q.dequeue();
+        case 1:
+            cout << "Enqueue Operation \nEnter an item to Enqueue in the Queue" << endl;
+            cin >> value;
+            q1.enqueue(value);
             break;
-        }
-        case 3:{
-            q.display();
+        case 2:
+            cout << "Dequeue Operation \nDequeued Value : " << q1.dequeue() << endl;
             break;
-        }
-        case 4:{
+        case 3:
+            if (q1.isEmpty())
+                cout << "Queue is Empty" << endl;
+            else
+                cout << "Queue is not Empty" << endl;
+            break;
+        case 4:
+            if (q1.isFull())
+                cout << "Queue is Full" << endl;
+            else
+                cout << "Queue is not Full" << endl;
+            break;
+        case 5:
+            cout << "Count Operation \nCount of items in Queue : " << q1.count() << endl;
+            break;
+        case 6:
+            cout << "Display Function Called - " << endl;
+            q1.display();
+            break;
+        case 7:
             system("cls");
             break;
+        default:
+            cout << "Enter Proper Option number " << endl;
         }
 
-    }
-}
+    } while (option != 0);
+
+    return 0;
 }
---
+++ insertingIntoArray.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+class Insertion
+{
+private:
+    int n;
+    int a[];
+
+public:
+    Insertion()
+    {
+        cout << "Enter the size of array -> ";
+        cin >> n;
+        a[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = 0;
+        }
+        cout << "<------------ Enter 0 for empty space ----------->" << endl
+             << endl;
+        cout << "Enter the elements of array -> "
+             << endl;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+    }
+    int check()
+    {
+        int c=0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == 0)
+            {
+                cout << "<- Free space available at index -> " << i << endl
+                     << endl;
+            }
+            else{
+                c+=1;
+            }
+
+        }
+        if(c==n){
+            cout<<"<- No free space ->"<<endl;
+        }
+        }
+    void ins()
+    {
+        int x;
+        int y;
+        cout << "Enter the element -> ";
+        cin >> x;
+        cout << "Enter the index where you want to add the element -> ";
+        cin >> y;
+        a[y] = x;
+    }
+    void display()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+            endl(cout);
+        }
+    }
+};
+int main()
+{
+    int j;
+    Insertion i;
+    do
+    {
+        cout << "Enter the option which you want to choose?\n1.Check space\n2.Insert element\n3.Display\n4.Clear Screen" << endl;
+        cin >> j;
+        cout << endl;
+        switch (j)
+        {
+        case 1:
+        {
+            i.check();
+            break;
+        }
+        case 2:
+        {
+            i.ins();
+            break;
+        }
+        case 3:
+        {
+            i.display();
+            break;
+        }
+        case 4:
+        {
+            system("cls");
+            break;
+        }
+        default:
+        {
+            cout << "!! Invalid Choice !!" << endl;
+        }
+        }
+    } while (1 > 0);
+    return 0;
+}
