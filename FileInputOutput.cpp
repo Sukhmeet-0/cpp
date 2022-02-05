034b1e99d771c6075665e7e2d027151ae7e2a80d
sukhmeet singh <sukhmeets111@gmail.com>
2022-02-05 12:47:40 +0530

    adding file operations

---
+++ FileInputOutput.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+
+int main(){
+    // char f[10];
+    // cout<<"Enter a file name: ";
+    // cin>>f;
+    //ofstream out(f);
+    // ofstream out;
+    // out.open("FileInputOutput.txt");
+    // string s="hi i am sukhmeet singh";
+    // string m="hi i am harmeet singh";
+    // out<<s<<endl<<m;
+    // out.close();
+    string fj="hiiiiiiiiiiiiii";
+    ofstream outa;
+    outa.open("FileInputOutput.txt");
+    outa<<fj;
+    outa.close();
+}
